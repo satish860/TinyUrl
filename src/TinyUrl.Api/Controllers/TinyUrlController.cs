48e5712c3ba41bbc3911dd51570b84fd0dd81c57
satish <satish860@gmail.com>
2022-01-25 14:29:42 +0530

    Controller Added the initial redirect

---
+++ src/TinyUrl.Api/Controllers/TinyUrlController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TinyUrl.Api.Controllers;
+
+[Route("api/[Controller]")]
+[ApiController]
+public class TinyUrlController : ControllerBase
+{
+    [HttpGet("{code}")]
+    // GET
+    public ActionResult Redirect(string code)
+    {
+        return Ok(code);
+    }
+}
